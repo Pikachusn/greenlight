a83215dcc33234cea88da07b642b8cfdaa59e8f0
pikachu <bearclawr@163.com>
2022-02-26 11:41:45 +0800

    【API Endpoints and RESTful Routing】

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,7 +21,7 @@ const version = "1.0.0"
 // configuration settings from command-line flags when the application starts.
 type config struct {
 	port int
-	env string
+	env  string
 }
 
 // Define an application struct to hold the dependencies for our HTTP Handlers, helpers,
@@ -40,12 +40,12 @@ func main() {
 	// default to using the port number 4000 and the environment "development" if no
 	// corresponding flags are provided.
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
-	flag.StringVar(&cfg.env, "env", "development",  "Environment (development|staging|production)")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 	flag.Parse()
 
 	// Initialize a new logger which writes messages to the standard out stream,
 	// prefixed with the current date and time.
-	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	// Declare an instance of the application struct, containing the config struct and
 	// the logger.
@@ -54,19 +54,14 @@ func main() {
 		logger: logger,
 	}
 
-	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
-	// to the healthcheckHandler method (which we will create in a moment).
-	mux := http.NewServeMux();
-	mux.HandleFunc("/v1/healthcheck", app.healthchechHandler);
-
 	// Declare a HTTP server with some sensible timeout settings, which listens on the
 	// port provided in the config struct and uses the servemux we created above as the
 	// handler.
 	srv := &http.Server{
-		Addr: fmt.Sprintf(":%d", cfg.port),
-		Handler: mux,
-		IdleTimeout: time.Minute,
-		ReadTimeout: 10 * time.Second,
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 30 * time.Second,
 	}
 
