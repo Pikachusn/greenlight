5d7db866a69018a23865f215bdebaf4992951d1a
pikachu <bearclawr@163.com>
2022-02-26 15:46:55 +0800

    【Creating a helper to read ID parameters】

---
+++ cmd/api/helper.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// Retrieve the "id" URL parameters from the current request context, then convert it to
+// an integer and return it. If the operation isn't successful, return 0 and an error.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id == 0 {
+		return 0, errors.New("invalid id paramter")
+	}
+
+	return id, nil
+}
