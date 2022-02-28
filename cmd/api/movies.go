3a2c4bd7449bb013e083a9f1614c2ac239b40064
Pikachu <bearclawr@163.com>
2022-02-28 22:25:49 +0800

    [Encoding Structs]

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"greenlight.alexedwards.net/internal/data"
 	"net/http"
+	"time"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint. For now we simply
@@ -21,5 +23,19 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response.
+	err = app.writeJOSN(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
