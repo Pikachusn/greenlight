046de90a07faa494aaebce29f71eb2a925459d1a
Pikachu <bearclawr@163.com>
2022-03-01 16:00:59 +0800

    [Advanced JSON Customization]
    Alternative #1 - Customizing the Movie struct

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,10 @@
 package data
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"time"
+)
 
 type Movie struct {
 	ID        int64     `json:"id"`                // Unique integer ID for the movie
@@ -10,4 +14,32 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"` // Movie runtime (in minutes)
 	Genres    []string  `json:"genres,omitempty"`  // Slice of genres for the movie (romance, comedy, etc.)
 	Version   int32     `json:"version"`           // The version number starts at 1 and will be incremented each time the movie information is updated
+}
+
+func (m Movie) MarshalJSON() ([]byte, error) {
+	var runtime string
+
+	if m.Runtime != 0 {
+		runtime = fmt.Sprintf("%d mins", m.Runtime)
+	}
+
+	aux := struct {
+		ID      int64    `json:"id"`
+		Title   string   `json:"title"`
+		Year    int32    `json:"year,omitempty"`
+		Runtime string   `json:"runtime,omitempty"`
+		Genres  []string `json:"genres,omitempty"`
+		Version int32    `json:"version"`
+	}{
+		// Set the values for the anonymous struct.
+		ID:      m.ID,
+		Title:   m.Title,
+		Year:    m.Year,
+		Runtime: runtime,
+		Genres:  m.Genres,
+		Version: m.Version,
+	}
+
+	// Encode the anonymous struct to JSON, and return it.
+	return json.Marshal(aux)
 }
